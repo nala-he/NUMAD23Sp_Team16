852a5abca628f9f84c574d69c949832d5efe5b4a
MaceeQi <maceeqi@gmail.com>
2023-03-01 22:38:09 -0500

    create login dialog to input username before continuing to A8

--- app/src/main/java/edu/northeastern/numad23sp_team16/LoginActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_team16/LoginActivity.java
@@ -6,20 +6,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText username;
     private Button loginButton;
+    private Button closeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        // Display login dialog
+        // Set up login dialog
         AlertDialog.Builder login = new AlertDialog.Builder(this);
 
         // Create layout inflater
@@ -27,7 +30,42 @@ public class LoginActivity extends AppCompatActivity {
         // Create view based on custom dialog
         View view = inflater.inflate(R.layout.login_dialog, null);
 
-        // Get username and login button from dialog
+        // Get username and login/close button from dialog
+        username = view.findViewById(R.id.usernameInput);
+        loginButton = view.findViewById(R.id.loginButton);
+        closeButton = view.findViewById(R.id.closeButton);
 
+        // Set view and ensure user cannot
+        login.setView(view);
+        login.setCancelable(false);
+
+        // Create login dialog
+        AlertDialog loginDialog = login.create();
+        loginDialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        loginDialog.show();
+
+
+        // Click on login button - checks database for user
+        // TODO: creates new user if username doesn't exist, logins as user if user exists
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get username from user
+                String user = username.getText().toString();
+
+                Toast.makeText(getApplicationContext(), user, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        // Click on close button - return to previous activity
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Return to previous activity
+                loginDialog.dismiss();
+                finish();
+                return;
+            }
+        });
     }
 }
