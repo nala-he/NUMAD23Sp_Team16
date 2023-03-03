436324f1915ea49caf8ff707cd17a603ef990d67
YuanKuang <98054860+greendress2022@users.noreply.github.com>
2023-03-02 23:37:10 -0500

    combined sticker page with Macee's StickItToEmActivity

--- app/src/main/java/edu/northeastern/numad23sp_team16/AboutActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_team16/AboutActivity.java
@@ -8,19 +8,13 @@ import android.view.View;
 import android.widget.Button;
 
 public class AboutActivity extends AppCompatActivity {
-    Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-        button = findViewById(R.id.sticker_btn);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(AboutActivity.this, StickerActivity.class));
-            }
-        });
+
     }
 
 
--- app/src/main/java/edu/northeastern/numad23sp_team16/StickItToEmActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_team16/StickItToEmActivity.java
@@ -1,16 +1,22 @@
 package edu.northeastern.numad23sp_team16;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class StickItToEmActivity extends AppCompatActivity {
 
     private final String CURRENT_USER = "CURRENT_USER";
     private String currentUser;
     private TextView currentlyLoggedIn;
+    private RecyclerView recyclerView;
+    private ArrayList<Sticker> stickerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +32,18 @@ public class StickItToEmActivity extends AppCompatActivity {
         // Set title for currently logged in user
         currentlyLoggedIn = findViewById(R.id.currentUserTitle);
         currentlyLoggedIn.setText("Currently Logged In: " + currentUser);
+
+        //display 4 stickers in recyclerview for user to send
+        stickerList = new ArrayList<>();
+        //initialize stickerList with four stickers
+        stickerList.add(new Sticker(R.drawable.giraffe));
+        stickerList.add(new Sticker(R.drawable.lion));
+        stickerList.add(new Sticker(R.drawable.gorilla));
+        stickerList.add(new Sticker(R.drawable.hedgehog));
+        //set the recycler view for stickers
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setAdapter(new StickerAdapter(stickerList));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
