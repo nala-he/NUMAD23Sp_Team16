3ce9fc93758cbc2498f046c8ef8e413717fc26ac
YuanKuang <98054860+greendress2022@users.noreply.github.com>
2023-03-02 19:39:53 -0500

    add a send sticker button in AboutActivity click this button, display 4 stickers in a recyclerview

--- app/src/main/java/edu/northeastern/numad23sp_team16/AboutActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_team16/AboutActivity.java
@@ -2,13 +2,26 @@ package edu.northeastern.numad23sp_team16;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AboutActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        button = findViewById(R.id.sticker_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AboutActivity.this, StickerActivity.class));
+            }
+        });
     }
+
+
 }
