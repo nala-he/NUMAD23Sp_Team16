2ae30eb5ddb026071e1fb78f26f292089f4534fe
MaceeQi <maceeqi@gmail.com>
2023-03-04 16:42:57 -0500

    store references to sticker, username, and date in view holder

--- app/src/main/java/edu/northeastern/numad23sp_team16/ReceivedStickerViewHolder.java
+++ app/src/main/java/edu/northeastern/numad23sp_team16/ReceivedStickerViewHolder.java
@@ -1,12 +1,24 @@
 package edu.northeastern.numad23sp_team16;
 
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class ReceivedStickerViewHolder extends RecyclerView.ViewHolder{
+
+    private ImageView sticker;
+    private TextView username;
+    private TextView date;
+
     public ReceivedStickerViewHolder(@NonNull View itemView) {
         super(itemView);
+
+        // Store references to sticker image, username, and date
+        this.sticker = itemView.findViewById(R.id.sticker_image);
+        this.username = itemView.findViewById(R.id.sender_name);
+        this.date = itemView.findViewById(R.id.sent_on);
     }
 }
