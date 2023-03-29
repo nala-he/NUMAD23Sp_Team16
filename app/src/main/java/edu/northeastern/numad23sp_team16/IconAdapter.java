91ad821bc6cbd1bc9f325448d0b855cb0c647a0d
MaceeQi <maceeqi@gmail.com>
2023-03-29 16:26:32 -0400

    create adapter for icons

--- app/src/main/java/edu/northeastern/numad23sp_team16/IconAdapter.java
+++ app/src/main/java/edu/northeastern/numad23sp_team16/IconAdapter.java
@@ -1,25 +1,42 @@
 package edu.northeastern.numad23sp_team16;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class IconAdapter extends RecyclerView.Adapter<IconViewHolder>{
+import java.util.List;
+
+import edu.northeastern.numad23sp_team16.models.Icon;
+
+public class IconAdapter extends RecyclerView.Adapter<IconViewHolder> {
+
+    Context context;
+    List<Icon> icons;
+
+    public IconAdapter(Context context, List<Icon> icons) {
+        this.context = context;
+        this.icons = icons;
+    }
 
     @NonNull
     @Override
     public IconViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        // Create view holder based on icon_view.xml
+        return new IconViewHolder(LayoutInflater.from(context).inflate(R.layout.icon_view, parent, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull IconViewHolder holder, int position) {
-
+        // Bind specific icon to views in view holder
+        holder.iconName.setText(icons.get(position).getIconName());
+        holder.icon.setImageResource(icons.get(position).getIconId());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return icons.size();
     }
 }
