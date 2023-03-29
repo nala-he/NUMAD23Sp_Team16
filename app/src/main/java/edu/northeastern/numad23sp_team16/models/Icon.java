7af2197839d9b22771e6e2a0136a5df239219133
MaceeQi <maceeqi@gmail.com>
2023-03-29 16:07:50 -0400

    add name attribute to icon model

--- app/src/main/java/edu/northeastern/numad23sp_team16/models/Icon.java
+++ app/src/main/java/edu/northeastern/numad23sp_team16/models/Icon.java
@@ -2,9 +2,15 @@ package edu.northeastern.numad23sp_team16.models;
 
 public class Icon {
     private int iconId;
+    private String iconName;
 
-    public Icon(int iconId) {
+    public Icon(int iconId, String iconName) {
         this.iconId = iconId;
+        this.iconName = iconName;
+    }
+
+    public String getIconName() {
+        return iconName;
     }
 
     public int getIconId() {
@@ -13,5 +19,9 @@ public class Icon {
 
     public void setIconId(int iconId) {
         this.iconId = iconId;
+    }
+
+    public void setIconName(String iconName) {
+        this.iconName = iconName;
     }
 }
