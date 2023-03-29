84b72e1c6693bb1a93d5c640fe24a7d5d41f3c7c
MaceeQi <maceeqi@gmail.com>
2023-03-29 11:10:50 -0400

    create temp button to create new goal

---
+++ app/src/main/java/edu/northeastern/numad23sp_team16/CreateNewGoalActivity.java
@@ -0,0 +1,14 @@
+package edu.northeastern.numad23sp_team16;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateNewGoalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_new_goal);
+    }
+}
--- app/src/main/java/edu/northeastern/numad23sp_team16/GoalForItActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_team16/GoalForItActivity.java
@@ -2,7 +2,9 @@ package edu.northeastern.numad23sp_team16;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class GoalForItActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class GoalForItActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goal_for_it);
+    }
+
+    public void startCreateNewGoalActivity(View view) {
+        startActivity(new Intent(GoalForItActivity.this, CreateNewGoalActivity.class));
     }
 }
