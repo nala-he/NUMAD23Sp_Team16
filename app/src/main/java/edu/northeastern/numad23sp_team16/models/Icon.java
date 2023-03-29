aedbef8997875873d5c6553e137dc6da511ca1ff
MaceeQi <maceeqi@gmail.com>
2023-03-29 18:05:10 -0400

    modify icon model class to include selected attribute

--- app/src/main/java/edu/northeastern/numad23sp_team16/models/Icon.java
+++ app/src/main/java/edu/northeastern/numad23sp_team16/models/Icon.java
@@ -3,10 +3,17 @@ package edu.northeastern.numad23sp_team16.models;
 public class Icon {
     private int iconId;
     private String iconName;
+    private boolean selected = false;
 
     public Icon(int iconId, String iconName) {
         this.iconId = iconId;
         this.iconName = iconName;
+    }
+
+    public Icon(int iconId, String iconName, boolean isSelected) {
+        this.iconId = iconId;
+        this.iconName = iconName;
+        this.selected = isSelected;
     }
 
     public String getIconName() {
@@ -17,11 +24,19 @@ public class Icon {
         return iconId;
     }
 
+    public boolean getSelected() {
+        return this.selected;
+    }
+
     public void setIconId(int iconId) {
         this.iconId = iconId;
     }
 
     public void setIconName(String iconName) {
         this.iconName = iconName;
+    }
+
+    public void setSelected(boolean isSelected) {
+        this.selected = isSelected;
     }
 }
