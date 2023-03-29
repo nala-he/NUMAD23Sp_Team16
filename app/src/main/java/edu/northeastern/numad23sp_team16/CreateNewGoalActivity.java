08f2c6db52dec1d16000278c0226193f481b0767
MaceeQi <maceeqi@gmail.com>
2023-03-29 13:22:43 -0400

    create custom action bar with back button for create new goal screen

--- app/src/main/java/edu/northeastern/numad23sp_team16/CreateNewGoalActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_team16/CreateNewGoalActivity.java
@@ -1,14 +1,24 @@
 package edu.northeastern.numad23sp_team16;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 public class CreateNewGoalActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new_goal);
+
+        // Set custom action bar
+        toolbar = findViewById(R.id.create_goal_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 }
