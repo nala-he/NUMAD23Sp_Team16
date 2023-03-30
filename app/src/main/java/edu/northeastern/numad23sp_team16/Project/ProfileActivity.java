63733a9345a1ce0ad291f167ef0f9a46ed7f840b
nala-he <hytdaisy@gmail.com>
2023-03-30 10:51:49 -0400

    revised format, added scrollview for landscape view

--- app/src/main/java/edu/northeastern/numad23sp_team16/Project/ProfileActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_team16/Project/ProfileActivity.java
@@ -3,15 +3,20 @@ package edu.northeastern.numad23sp_team16.Project;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.RadioButton;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 import edu.northeastern.numad23sp_team16.R;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,10 +30,22 @@ public class ProfileActivity extends AppCompatActivity {
         actionBar.setHomeAsUpIndicator(R.drawable.back_button);
         actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#D9D9D9")));
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        // set user profile values to textInputEditText
+        TextInputEditText username_input = findViewById(R.id.username_input);
+        TextInputEditText password_input = findViewById(R.id.password_input);
+        TextInputEditText email_input = findViewById(R.id.email_input);
+        TextInputEditText petname_input = findViewById(R.id.petname_input);
+        RadioButton dog_button = findViewById(R.id.dog_radio_button);
+
+        //TODO: change to realtime data later
+        username_input.setText("FirstUser");
+        password_input.setText("FirstUser");
+        email_input.setText("firstUser@gmail.com");
+        petname_input.setText("Juni");
     }
 
-    // this event will enable the back
-    // function to the button on press
+    // this event will enable the back function to the back button on press in customized action bar
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
