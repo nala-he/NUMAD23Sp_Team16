ad5d3d38bed77545557f9490760e1a055544ae4e
MaceeQi <maceeqi@gmail.com>
2023-04-01 19:38:59 -0400

    set temp pet name and image, add image of pets with rounded corners (more clear than dog_small and cat_small)

--- app/src/main/java/edu/northeastern/numad23sp_team16/Project/ProgressActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_team16/Project/ProgressActivity.java
@@ -3,14 +3,29 @@ package edu.northeastern.numad23sp_team16.Project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import edu.northeastern.numad23sp_team16.R;
 
 public class ProgressActivity extends AppCompatActivity {
 
+    TextView petName;
+    ImageView petImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progress);
+
+        // Get pet name and image from view
+        petName = findViewById(R.id.progress_pet_name);
+        petImage = findViewById(R.id.progress_pet_image);
+
+        // TODO: replace with pet name and image from database
+        // Set pet name and image for current user
+        petName.setText("Juni");
+        petImage.setImageResource(R.drawable.dog_small);
     }
 }
