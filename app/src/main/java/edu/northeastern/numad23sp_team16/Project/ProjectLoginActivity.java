1f56231ca6a36446095723b14e9006320aec5dfa
YuanKuang <98054860+greendress2022@users.noreply.github.com>
2023-04-04 16:17:36 -0400

    add my_button_background.xml and customize toolbar for startActivity

---
+++ app/src/main/java/edu/northeastern/numad23sp_team16/Project/ProjectLoginActivity.java
@@ -0,0 +1,16 @@
+package edu.northeastern.numad23sp_team16.Project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.northeastern.numad23sp_team16.R;
+
+public class ProjectLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_login);
+    }
+}
