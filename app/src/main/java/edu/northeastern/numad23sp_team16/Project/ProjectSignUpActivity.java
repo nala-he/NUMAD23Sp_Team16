3e8a347ab596a6e3f96d50159cfb38ae01194b28
YuanKuang <98054860+greendress2022@users.noreply.github.com>
2023-04-04 17:06:03 -0400

    toolbar in ProjectSignUpActivity
    add toolbar in ProjectSignUpActivity, click back button returns to parent activity

--- app/src/main/java/edu/northeastern/numad23sp_team16/Project/ProjectSignUpActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_team16/Project/ProjectSignUpActivity.java
@@ -1,8 +1,12 @@
 package edu.northeastern.numad23sp_team16.Project;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import edu.northeastern.numad23sp_team16.R;
 
@@ -12,5 +16,16 @@ public class ProjectSignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project_sign_up);
+        //customize tool bar
+        Toolbar toolbar = findViewById(R.id.signup_act_toolbar);
+        setSupportActionBar(toolbar);
+        //click back button
+        ImageView backButton = findViewById(R.id.back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
