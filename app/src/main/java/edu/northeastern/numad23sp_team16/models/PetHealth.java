b08d5b8ff2dd2a27b9e99e34264b7ee80f6f2089
MaceeQi <maceeqi@gmail.com>
2023-04-19 16:35:53 -0400

    remove total health attribute for PetHealth - not needed

--- app/src/main/java/edu/northeastern/numad23sp_team16/models/PetHealth.java
+++ app/src/main/java/edu/northeastern/numad23sp_team16/models/PetHealth.java
@@ -4,20 +4,17 @@ public class PetHealth {
     private String owner;
     private float averageHealth;
     private String creationDate;
-    private float totalHealth;
 
     public PetHealth(String owner, String creationDate) {
         this.owner = owner;
         this.averageHealth = 100;
         this.creationDate = creationDate;
-        this.totalHealth = 0;
     }
 
-    public PetHealth(String owner, float averageHealth, String creationDate, float totalHealth) {
+    public PetHealth(String owner, float averageHealth, String creationDate) {
         this.owner = owner;
         this.averageHealth = averageHealth;
         this.creationDate = creationDate;
-        this.totalHealth = totalHealth;
     }
 
     // Default constructor required for calls to DataSnapshot.getValue(PetHealth.class)
@@ -36,7 +33,4 @@ public class PetHealth {
         return this.creationDate;
     }
 
-    public float getTotalHealth() {
-        return this.totalHealth;
-    }
 }
